daf908545610e23f6d49b19fdc3f1f02134d2651
Adrien Toussaint <toussa137u@iutsd-127-13.ad.univ-lorraine.fr>
2018-01-22 16:58:38 +0100

    ajout rock

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component} from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController } from 'ionic-angular';
 
 
 
@@ -9,35 +9,58 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
   score = 0;
-  typehache = {"zero":
-                {"power":0.001,
+  typehache = {0:
+                {"power":1,
                 "mat":"mousse",
                 "price":0
                 },
-               "un":
-                  {"power":0.05,
+              1:
+                  {"power":10,
                    "mat":"bois",
-                   "price":1
+                   "price":50
                   },
-               "deux":
-                  {"power":0.1,
+              2:
+                  {"power":100,
                   "mat":"pierre",
-                  "price":10
+                  "price":1000
                   },
-               "trois":
-                  {"power":0.5,
+              3:
+                  {"power":1000,
                   "mat":"fer",
-                  "price":50
+                  "price":10000
+                  },
+              4:
+                  {"power":10000,
+                  "mat":"acier",
+                  "price":1000000
+                  },
+              5:
+                  {"power":100000,
+                  "mat":"uranium",
+                  "price":10000000
                   }
               };
   diff = 0;
-  hache = "zero";
+  hache = 0;
   objectKeys = Object.keys;
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private alertCtrl: AlertController) {
 
   }
   clic(){
     this.score+=this.typehache[this.hache].power;
-}
+  }
+  pay(price:number){
+    if(this.score>=price){
+      this.score-=price;
+      this.hache +=1;
+    }else{
+      let alert = this.alertCtrl.create({
+        title:'Vous êtes trop pauvre !',
+        subTitle:'Il vous manque '+(price-this.score)+' rondins pour améliorer votre hache...',
+        buttons:['Je retourne farmer']
+      });
+      alert.present();
+    }
+  }
 }
 
--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -1,37 +1,63 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-list',
   templateUrl: 'list.html'
 })
 export class ListPage {
-  selectedItem: any;
-  icons: string[];
-  items: Array<{title: string, note: string, icon: string}>;
+  score = 0;
+  typehache = {0:
+                {"power":1,
+                "mat":"mousse",
+                "price":0
+                },
+              1:
+                  {"power":10,
+                   "mat":"bois",
+                   "price":50
+                  },
+              2:
+                  {"power":100,
+                  "mat":"pierre",
+                  "price":1000
+                  },
+              3:
+                  {"power":1000,
+                  "mat":"fer",
+                  "price":10000
+                  },
+              4:
+                  {"power":10000,
+                  "mat":"acier",
+                  "price":1000000
+                  },
+              5:
+                  {"power":100000,
+                  "mat":"uranium",
+                  "price":10000000
+                  }
+              };
+  diff = 0;
+  hache = 0;
+  objectKeys = Object.keys;
+  constructor(public navCtrl: NavController, private alertCtrl: AlertController) {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-    // If we navigated to this page, we will have an item available as a nav param
-    this.selectedItem = navParams.get('item');
-
-    // Let's populate this page with some filler content for funzies
-    this.icons = ['flask', 'wifi', 'beer', 'football', 'basketball', 'paper-plane',
-    'american-football', 'boat', 'bluetooth', 'build'];
-
-    this.items = [];
-    for (let i = 1; i < 11; i++) {
-      this.items.push({
-        title: 'Item ' + i,
-        note: 'This is item #' + i,
-        icon: this.icons[Math.floor(Math.random() * this.icons.length)]
-      });
-    }
   }
-
-  itemTapped(event, item) {
-    // That's right, we're pushing to ourselves!
-    this.navCtrl.push(ListPage, {
-      item: item
-    });
+  clic(){
+    this.score+=this.typehache[this.hache].power;
+  }
+  pay(price:number){
+    if(this.score>=price){
+      this.score-=price;
+      this.hache +=1;
+    }else{
+      let alert = this.alertCtrl.create({
+        title:'Vous êtes trop pauvre !',
+        subTitle:'Il vous manque '+(price-this.score)+' rondins pour améliorer votre hache...',
+        buttons:['Je retourne farmer']
+      });
+      alert.present();
+    }
   }
 }
