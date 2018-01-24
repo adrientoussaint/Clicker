e2bd4a66102cee6ed74bda74fec56bfb11457338
Adrien Toussaint <adrien@MacBook-Pro-de-Adrien-3.local>
2018-01-24 22:55:57 +0100

    ajout auto-incr

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -11,43 +11,85 @@ import { StatusBar } from '@ionic-native/status-bar';
 })
 export class HomePage {
   score = 0;
+  rocks = 0;
+  hache = 0;
+  task = 0;
+  objectKeys = Object.keys;
   typehache = {0:
                 {"power":1,
                 "mat":"mousse",
-                "price":0
+                "price":0,
+                "item":"hache"
                 },
               1:
                   {"power":10,
                    "mat":"bois",
-                   "price":50
+                   "price":50,
+                   "item":"hache"
                   },
               2:
                   {"power":100,
                   "mat":"pierre",
-                  "price":1000
+                  "price":1000,
+                  "item":"hache"
                   },
               3:
                   {"power":1000,
                   "mat":"fer",
-                  "price":10000
+                  "price":10000,
+                  "item":"hache"
                   },
               4:
                   {"power":10000,
                   "mat":"acier",
-                  "price":1000000
+                  "price":1000000,
+                  "item":"hache"
                   },
               5:
                   {"power":100000,
                   "mat":"uranium",
-                  "price":10000000
+                  "price":10000000,
+                  "item":"hache"
                   }
               };
-  diff = 0;
-  rocks = 0;
-  hache = 0;
-  objectKeys = Object.keys;
+ 
+              outilbois={0:
+                {"power":0.01,
+                "price":15,
+                "name":"Tronçonneuse électrique",
+                "item":"outil",
+                "quantite":0
+                },1:
+                {"power":0.1,
+                "price":35,
+                "name":"Tronçonneuse à essence",
+                "item":"outil",
+                "quantite":0
+                }
+              };
   constructor(public navCtrl: NavController,public statusBar: StatusBar, private alertCtrl: AlertController,private storage: Storage) {
 
+  }
+  
+  autoincrement(val:number){
+    this.score+=val;
+    this.storage.set("wood",this.score);
+  }
+
+  ionViewDidLoad(){
+    this.task = setInterval( () => {
+      for(let outil in this.outilbois){
+        this.autoincrement(this.outilbois[outil].power*this.outilbois[outil].quantite);
+      }
+    }, 1000); 
+  }
+
+  ionViewDidLeave(){
+    this.task = setInterval( () => {
+      for(let outil in this.outilbois){
+        this.autoincrement(this.outilbois[outil].power*this.outilbois[outil].quantite);
+      }
+    }, 1000); 
   }
 
   ionViewDidEnter(){
@@ -72,28 +114,43 @@ export class HomePage {
         this.hache = 0;
       }
     });
+    for(let outil in this.outilbois){
+      this.storage.get('outil'+outil).then((val) => {
+        if(val!=null){
+          this.outilbois[outil].quantite = val;
+        }else{
+          this.outilbois[outil].quantite  = 0;
+        }
+      });
+    }
+   
   }
 
   clic(){
-    this.score+=this.typehache[this.hache].power;
+    this.score+=(this.typehache[this.hache].power*1.15);
     this.storage.set('wood', this.score);
-    
   }
-  pay(price:number){
+
+  pay(price:number,typeachat:string){
     if(this.score>=price){
       this.score-=price;
       this.storage.set('wood',this.score);
-
-      this.hache +=1;
-      this.storage.set('hache', this.hache);
-    }else{
+      if(typeachat=="hache"){
+        this.hache +=1;
+        this.storage.set('hache', this.hache);  
+      }else{
+        this.outilbois[typeachat].quantite+=1;
+        this.storage.set('outil'+typeachat, this.outilbois[typeachat].quantite);
+      }
+      }else{
       let alert = this.alertCtrl.create({
         title:'Vous êtes trop pauvre !',
-        subTitle:'Il vous manque '+(price-this.score)+' rondins pour améliorer votre hache...',
+        subTitle:'Il vous manque '+(price-this.score)+' rondins pour acheter cette amélioration...',
         buttons:['Je retourne farmer']
       });
       alert.present();
     }
   }
+ 
 }
 
