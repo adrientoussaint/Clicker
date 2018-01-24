bcef440bed01d6c965976bea754fbca1ef9f45db
Adrien Toussaint <toussa137u@iutsd-127-13.ad.univ-lorraine.fr>
2018-01-24 11:26:25 +0100

    ajout storage

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component} from '@angular/core';
 import { NavController, AlertController } from 'ionic-angular';
+import { Storage } from '@ionic/storage';
+import { StatusBar } from '@ionic-native/status-bar';
 
 
 
@@ -41,18 +43,49 @@ export class HomePage {
                   }
               };
   diff = 0;
+  rocks = 0;
   hache = 0;
   objectKeys = Object.keys;
-  constructor(public navCtrl: NavController, private alertCtrl: AlertController) {
+  constructor(public navCtrl: NavController,public statusBar: StatusBar, private alertCtrl: AlertController,private storage: Storage) {
 
   }
+
+  ionViewDidEnter(){
+    this.storage.get('wood').then((val) => {
+      if(val!=null){
+        this.score = val;
+      }else{
+        this.score = 0;
+      }
+    }); 
+    this.storage.get('rock').then((val) => {
+      if(val!=null){
+        this.rocks = val;
+      }else{
+        this.rocks = 0;
+      }
+    });
+    this.storage.get('hache').then((val) => {
+      if(val!=null){
+        this.hache = val;
+      }else{
+        this.hache = 0;
+      }
+    });
+  }
+
   clic(){
     this.score+=this.typehache[this.hache].power;
+    this.storage.set('wood', this.score);
+    
   }
   pay(price:number){
     if(this.score>=price){
       this.score-=price;
+      this.storage.set('wood',this.score);
+
       this.hache +=1;
+      this.storage.set('hache', this.hache);
     }else{
       let alert = this.alertCtrl.create({
         title:'Vous êtes trop pauvre !',
--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams, AlertController } from 'ionic-angular';
+import { Storage } from '@ionic/storage';
 
 @Component({
   selector: 'page-list',
@@ -7,54 +8,99 @@ import { NavController, NavParams, AlertController } from 'ionic-angular';
 })
 export class ListPage {
   score = 0;
-  typehache = {0:
+  typepioche = {0:
                 {"power":1,
                 "mat":"mousse",
-                "price":0
+                "price":0,
+                "pricebois":20000
                 },
               1:
                   {"power":10,
                    "mat":"bois",
-                   "price":50
+                   "price":50,
+                   "pricebois":200000
                   },
               2:
                   {"power":100,
                   "mat":"pierre",
-                  "price":1000
+                  "price":1000,
+                  "pricebois":2000000
                   },
               3:
                   {"power":1000,
                   "mat":"fer",
-                  "price":10000
+                  "price":10000,
+                  "pricebois":3000000
                   },
               4:
                   {"power":10000,
                   "mat":"acier",
-                  "price":1000000
+                  "price":1000000,
+                  "pricebois":10000000
                   },
               5:
                   {"power":100000,
                   "mat":"uranium",
-                  "price":10000000
+                  "price":1000000000000,
+                  "pricebois":350000000000
                   }
               };
   diff = 0;
-  hache = 0;
+  pioche = -1;
+  woods = 0;
   objectKeys = Object.keys;
-  constructor(public navCtrl: NavController, private alertCtrl: AlertController) {
+  constructor(public navCtrl: NavController, private alertCtrl: AlertController, private storage: Storage) {
 
   }
-  clic(){
-    this.score+=this.typehache[this.hache].power;
+  
+  ionViewDidEnter(){
+    this.storage.get('rock').then((val) => {
+      if(val!=null){
+        this.score = val;
+      }else{
+        this.score = 0;
+      }
+    });    
+    this.storage.get('wood').then((val) => {
+      if(val!=null){
+        this.woods = val;
+      }else{
+        this.woods = 0;
+      }
+    }); 
+    this.storage.get('pioche').then((val) => {
+      if(val!=null){
+        this.pioche = val;
+      }else{
+        this.pioche = -1;
+      }
+    });       
   }
-  pay(price:number){
-    if(this.score>=price){
+  ionViewDidLeave(){
+    this.storage.set('wood',this.woods);
+  }
+
+  clic(){
+    if(this.pioche!=-1){
+      this.score+=this.typepioche[this.pioche].power;
+      this.storage.set('rock', this.score);
+    }  
+  }
+
+  pay(price:number,pricebois:number){
+    if(this.score>=price && this.woods>=pricebois){
       this.score-=price;
-      this.hache +=1;
+      this.storage.set('rock', this.score);
+
+      this.woods-=pricebois;
+      this.storage.set('wood', this.woods);
+      
+      this.pioche +=1;
+      this.storage.set('pioche', this.pioche);
     }else{
       let alert = this.alertCtrl.create({
         title:'Vous êtes trop pauvre !',
-        subTitle:'Il vous manque '+(price-this.score)+' rondins pour améliorer votre hache...',
+        subTitle:'Il vous manque '+(price-this.score)+' tas et '+(pricebois-this.woods)+' pour améliorer votre pioche...',
         buttons:['Je retourne farmer']
       });
       alert.present();
